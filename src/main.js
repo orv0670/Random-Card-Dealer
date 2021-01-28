b451dc954467e91d3f3fe50e08b8bdf2309e700c
orv0670 <orv0670@gmail.com>
2021-01-28 17:24:48 +0000

    primera version

--- src/main.js
+++ src/main.js
@@ -2,9 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var numeroDeCarta = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "k",
+    "Q",
+    "A"
+  ];
+  var suit = ["&#9824", "&#9829", "&#9827", "&#9830"];
+  var numerosAleatorios =
+    numeroDeCarta[Math.floor(Math.random() * numeroDeCarta.length)];
+  var randomSuit = suit[this.Math.floor(Math.random() * suit.length)];
+
+  if (randomSuit == "&#9829" || randomSuit == "&#9830") {
+    document.querySelector(".carta").style.color = "red";
+  }
+
+  document.querySelector(".suit1").innerHTML = randomSuit;
+  document.querySelector(".valor").innerHTML = numerosAleatorios;
+  document.querySelector(".suit2").innerHTML = randomSuit;
 };
